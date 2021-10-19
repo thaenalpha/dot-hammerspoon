6c327ba355432aa50c1c1983721df173f1ea3915
Eason0210 <aqua0210@163.com>
2021-10-19 23:22:39 +0800

    adjust keybindings

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ local mash = {"ctrl", "cmd"}
 
 -- https://github.com/dsanson/hs.tiling
 local tiling = require "hs.tiling"
-hotkey.bind(mash, "i", function() tiling.cycleLayout() end)
-hotkey.bind(mash, "l", function() tiling.cycle(1) end)
-hotkey.bind(mash, "j", function() tiling.cycle(-1) end)
-hotkey.bind(mash, "k", function() tiling.promote() end)
-hotkey.bind(mash, "f", function() tiling.goToLayout("fullscreen") end)
+hotkey.bind(mash, "'", function() tiling.cycleLayout() end)
+-- hotkey.bind(mash, "l", function() tiling.cycle(1) end)
+-- hotkey.bind(mash, "j", function() tiling.cycle(-1) end)
+-- hotkey.bind(mash, "k", function() tiling.promote() end)
+hotkey.bind(mash, ";", function() tiling.goToLayout("fullscreen") end)
 
 tiling.set('layouts', {
   'fullscreen', 'main-vertical', 'gp-vertical', 'columns'
