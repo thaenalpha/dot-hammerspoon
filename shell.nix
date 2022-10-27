83410c7087eefab24aeab2d1bbf3fa3b140953e2
Nopanun Loachunhanun <nopanun@pm.me>
2022-10-27 18:15:26 +0700

    dev(nix-shell): pin pkgs to unstable channel commit
    <nixpkgs> -> NixOs/nixpkgs@1c6eb4876f71

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1c6eb4876f71e8903ae9f73e6adf45fdbebc0292.tar.gz") {} }:
 pkgs.mkShell {
   buildInputs = with pkgs; [ lua luaPackages.luacheck ];
 }
